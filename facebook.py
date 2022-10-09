fc763fc1f3bc763e074282b421d43e15c9f8711e
Fanan-soufiane <115195347+Fanan-soufiane@users.noreply.github.com>
2022-10-10 00:22:29 +0100

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,31 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;32m')
+    os.system ('clear')
+    print ("██╗░░██╗░█████╗░░█████╗░██╗░░██╗░░░░░███████╗██████╗░")
+    print ("██║░░██║██╔══██╗██╔══██╗██║░██╔╝░░░░░██╔════╝██╔══██╗ ")
+    print ("███████║███████║██║░░╚═╝█████═╝░░░░░░█████╗░░██████╦╝")
+    print ("██╔══██║██╔══██║██║░░██╗██╔═██╗░░░░░░██╔══╝░░██╔══██╗")
+    print( "██║░░██║██║░░██║╚█████╔╝██║░╚██╗░░░░░██║░░░░░██████╦╝")
+    print ("╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝░░░░░╚═╝░░░░░╚═════╝░")
+    time.sleep(0.2)
+    print ('\033[1;31m    1/1 دﺤاو بﺎﺴﺤ قاﺮﺘﺧإ كﻧﮑﻤﯾ VIP  تﺴﻟ تﻧأ')
+    fx9 = input (" \033[1;33m  ﺔﯾﺤﺿ بﺎﺴﺤ طﺑار  ==> \033[1;32m")
+    time.sleep(0.2)
+    if fx9 == 'https://www.facebook.com/profile.php?id=100070099311808' :
+            os.system ('python rot.py')
+            restart ()
+    elif fx9 == 'Yasser YL' :
+            os.system ('python rot.py')
+main (main)
+home_main ()
